61acaad80dfa320e17b71f93fbb54b398d195c6d
Justin <justindelabarra@gmail.com>
2022-10-14 17:21:46 -0500

    Update infra.tf

--- clase1/parte1/infra.tf
+++ clase1/parte1/infra.tf
@@ -2,21 +2,21 @@ provider "azurerm" {
   # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
   features {}
 }
-resource "azurerm_resource_group" "test" {
-  name     = "RGAzureDemo"
+resource "azurerm_resource_group" "rgtfstate" {
+  name     = "rgtfstateprod"
   location = "East US"
   tags = {
     Ambiente = "Desarrollo"
   }
 }
 resource "azurerm_storage_account" "stgid" {
-  name                     = "stgjoyapuazure"
-  resource_group_name      = "${azurerm_resource_group.test.name}"
-  location                 = "${azurerm_resource_group.test.location}"
+  name                     = "satfstateprod"
+  resource_group_name      = "${azurerm_resource_group.rgtfstate.name}"
+  location                 = "${azurerm_resource_group.rgtfstate.location}"
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
   tags = {
-    Ambiente = "Desarrollo"
+    env = "Desarrollo"
   }
 }
