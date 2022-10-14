d4371e32c3b2b0242bf4b81db89fac7493a73ca2
Justin <justindelabarra@gmail.com>
2022-10-14 17:27:33 -0500

    Update infra.tf

--- clase1/parte2/infra.tf
+++ clase1/parte2/infra.tf
@@ -2,18 +2,18 @@ provider "azurerm" {
   # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
   features {}
 }
-resource "azurerm_resource_group" "test" {
-  name     = "RG${local.alias}"
+resource "azurerm_resource_group" "rglovisa" {
+  name     = "rg${local.alias}"
   location = "${local.region}"
   tags = {
     Ambiente = "Desarrollo"
   }
 }
 
-resource "azurerm_app_service_plan" "main" {
+resource "azurerm_app_service_plan" "appsrvplan" {
   name                = "az${local.alias}-asp"
-  location            = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
+  location            = "${azurerm_resource_group.rglovisa.location}"
+  resource_group_name = "${azurerm_resource_group.rglovisa.name}"
 
   sku {
     tier = "Standard"
@@ -23,8 +23,8 @@ resource "azurerm_app_service_plan" "main" {
 
 resource "azurerm_app_service" "main" {
   name                = "az${local.alias}-web"
-  location            = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  app_service_plan_id = "${azurerm_app_service_plan.main.id}"
+  location            = "${azurerm_resource_group.rglovisa.location}"
+  resource_group_name = "${azurerm_resource_group.rglovisa.name}"
+  app_service_plan_id = "${azurerm_app_service_plan.appsrvplan.id}"
 
 }
